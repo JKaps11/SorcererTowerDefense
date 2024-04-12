fc735bcafdac2fce01a2e089967b0aed1f49f45c
Jkaps11 <kapsjosh11@gmail.com>
2024-04-12 19:36:20 -0400

    adding opening image

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -I src/include -L src/lib -o main main.c -l mingw32 -l SDL2main -l SDL2
+	g++ -I src/include -L src/lib -o main main.c -l mingw32 -l SDL2main -l SDL2 -l SDL2_image
--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_image.h>
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0)
     {
         printf("error initializing SDL: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* win = SDL_CreateWindow("Arcane Tower Defense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+    SDL_Window* win = SDL_CreateWindow("Sorcerer Tower Defense", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
 
     if (!win)
     {
@@ -27,7 +29,37 @@ int main(int argc, char *argv[])
         SDL_Quit();
     }
 
+    SDL_Surface* surface = IMG_Load("src\\assets\\Main_Menu.gif");
+    if (!surface)
+    {
+        printf("error creating surface: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    SDL_FreeSurface(surface);
+    if(!tex)
+    {
+        printf("error creating texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_RenderClear(rend);
+
+    SDL_RenderCopy(rend, tex, NULL, NULL);
+
+    SDL_RenderPresent(rend);
+
     SDL_Delay(5000);
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
